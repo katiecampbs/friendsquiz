ca03ec01930f80849403f0ff9ddc079412479744
katiecampbs <>
2023-11-04 14:53:38 +0000

    Add list of questions and answers in javascript

---
+++ assets/javascript/script.js
@@ -0,0 +1,102 @@
+let myQuestions = [
+    {
+        question: "What is the name of Ross and Monica's dog when they were kids?",
+        answers: {
+            a: 'Buster',
+            b: 'Fluffy',
+            c: 'Rex',
+            d: 'Rover',
+        },
+        correctAnswer: 'Buster'
+    },
+    {
+        question: "Which character famously yelled, 'We were on a break!' during the series?",
+        answers: {
+            a: 'Ross',
+            b: 'Chandler',
+            c: 'Joey',
+            d: 'Phoebe',
+        },
+        correctAnswer: 'Ross'
+    },
+    {
+        question: "What is the name of the coffee shop where the friends frequently hang out?",
+        answers: {
+            a: 'Central Perk',
+            b: 'Java Jolt',
+            c: 'Espresso Emporium',
+            d: 'The Percolator',
+        },
+        correctAnswer: 'Central Perk'
+    },
+    {
+        question: "Who played the role of Ross Geller in the TV show 'Friends'?",
+        answers: {
+            a: 'Matt LeBlanc',
+            b: 'Matthew Perry',
+            c: 'David Schwimmer',
+            d: 'Courteney Cox',
+        },
+        correctAnswer: 'David Schwimmer'
+    },
+    {
+        question: "What is the profession of Ross Geller in the show?",
+        answers: {
+            a: 'Paleontologist',
+            b: 'Chef',
+            c: 'Actor',
+            d: 'Accountant',
+        },
+        correctAnswer: 'Paleontologist'
+    },
+    {
+        question: "Which character briefly lived with a woman named Janice, known for her distinctive laugh?",
+        answers: {
+            a: 'Monica',
+            b: 'Chandler',
+            c: 'Phoebe',
+            d: 'Joey',
+        },
+        correctAnswer: 'Chandler'
+    },
+    {
+        question: "Which character is known for her famous song, 'Smelly Cat'?",
+        answers: {
+            a: 'Monica',
+            b: 'Phoebe',
+            c: 'Rachel',
+            d: 'Janice',
+        },
+        correctAnswer: 'Phoebe'
+    },
+    {
+        question: "What is the name of Ross and Monica's father?",
+        answers: {
+            a: 'Bob',
+            b: 'Jack',
+            c: 'Richard',
+            d: 'George',
+        },
+        correctAnswer: 'Jack'
+    },
+    {
+        question: "What does Joey not share?",
+        answers: {
+            a: 'His apartment',
+            b: 'His food',
+            c: 'His clothes',
+            d: 'His acting tips',
+        },
+        correctAnswer: 'His food'
+    },
+    {
+        question: "What is the name of the coffee shop employee who frequently serves the friends at Central Perk?",
+        answers: {
+            a: 'Gunther',
+            b: 'Walter',
+            c: 'Roger',
+            d: 'Stever',
+        },
+        correctAnswer: 'Gunther'
+    }
+];

eed2d875498e7690e72aeb63caf238f9252dd2ca
katiecampbs <>
2023-11-23 16:22:51 +0000

    Create function that runs round two of questions, and add a new button as part of the inner html on the score container that triggers this function when clicked

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,6 +2,8 @@
 import quizData from './questions.js';
 
 const questionsOne = quizData.questionListOne;
+const questionsTwo = quizData.questionListTwo;
+const questionsThree = quizData.questionListThree;
 let questionElement, buttons, currentQuestionIndex, score, scoreContainer;
 
 function runRoundOne() {
@@ -56,7 +58,65 @@ function runRoundOne() {
             } else {
                 // Quiz is completed, replace the content with the score
                 scoreContainer = document.getElementById("score-container");
-                scoreContainer.innerHTML = `<h1>Quiz Score</h1><p>You scored: ${score} out of 5!</p>`;
+                scoreContainer.innerHTML = `<h1>Quiz Score</h1><p>You scored: ${score} out of 5!</p><button class="btn--restart" id="start-round-two-btn">Start round two</button>`;
+            }
+        }, 1000);
+    }
+};
+
+function runRoundTwo() {
+    //Insert the first question from the question list into the paragraph with an id of "question"
+    questionElement.textContent = questionsTwo[0].question;
+
+    //Cycle through the answers in the first question on the list and inserts an answer as text content to each of the buttons
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].textContent = questionsTwo[0].answers[i];
+    }
+    /**
+     * Run the checkAnswer function in the event that any of the buttons are clicked
+    */
+    buttons.forEach(function (button, index) {
+        button.addEventListener("click", function () {
+            const answer = questionsTwo[currentQuestionIndex].answers[index];
+            checkAnswer(answer, button);
+        });
+    });
+
+    /** Reset the answer button colour after it has turned red or green to show if answer is correct */
+    function resetButtonColors() {
+        buttons.forEach(function (button) {
+            button.style.backgroundColor = ""; // Reset button color
+        });
+    }
+    /**
+     * Check the answer on the button clicked against the answer in the questions list and displays an alert for the user
+     */
+    function checkAnswer(answer, button) {
+        let currentQuestion = questionsTwo[currentQuestionIndex];
+        if (answer === currentQuestion.correctAnswer) {
+            button.style.backgroundColor = "green";
+            score = ++score;
+        } else {
+            button.style.backgroundColor = "red";
+        }
+
+        // Reset button colors using a function after a short delay, and then move to the next question
+        setTimeout(function () {
+            resetButtonColors();
+
+            // Increment the question list by one
+            currentQuestionIndex++;
+
+            if (currentQuestionIndex < questionsTwo.length) {
+                // Display the next question
+                questionElement.textContent = questionsTwo[currentQuestionIndex].question;
+                for (let i = 0; i < buttons.length; i++) {
+                    buttons[i].textContent = questionsTwo[currentQuestionIndex].answers[i];
+                }
+            } else {
+                // Quiz is completed, replace the content with the score
+                scoreContainer = document.getElementById("score-container");
+                scoreContainer.innerHTML = `<h1>Quiz Score</h1><p>On round two you scored: ${score} out of 5!</p>`;
             }
         }, 1000);
     }
@@ -70,14 +130,29 @@ document.addEventListener("DOMContentLoaded", function () {
     scoreContainer = document.getElementById("score-container"); // Initialize score container
     score = 0;
     runRoundOne();
+
+    // Listen for the refresh button to be clicked. If clicked, it triggers the quiz to start from the beginning
+    let refreshBtn = document.getElementById("btn-refresh");
+    refreshBtn.addEventListener("click", handleClick);
+
+    // Set up event delegation for the "Start round two" button
+    document.addEventListener("click", function (event) {
+        // Check if the clicked element is the "Start round two" button
+        if (event.target.id === "start-round-two-btn") {
+            // Reset variables and run the second round
+            score = 0;
+            currentQuestionIndex = 0;
+            scoreContainer.innerHTML = ""; // Clear score container
+            runRoundTwo();
+        }
+    });
 });
 
-// Listen for the refresh button to be clicked. If clicked it triggers the quiz to start from the beginning
-let refreshBtn = document.getElementById("btn-refresh");
 function handleClick() {
     score = 0;
     currentQuestionIndex = 0;
     scoreContainer.innerHTML = ""; // Clear score container
     runRoundOne();
 }
-refreshBtn.addEventListener("click", handleClick);
+
+
